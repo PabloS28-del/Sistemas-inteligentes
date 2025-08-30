89086abc333366f8bdbeecdb84dc52f2c459116b
Pau1inh0 <yopablo1234321@gmail.com>
2025-08-29 22:55:16 -0500

    Netlify: base=/, import de global.css en Layout y netlify.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-
 import vue from "@astrojs/vue";
-
-
 import tailwindcss from "@tailwindcss/vite";
 
-
-// https://astro.build/config
 export default defineConfig({
+  base: "/",                // <- importante en Netlify
   integrations: [vue()],
-  base: "SI-FISI-G4-2025/",
-  vite: {
-    plugins: [tailwindcss()],
-  }
+  vite: { plugins: [tailwindcss()] },
 });
